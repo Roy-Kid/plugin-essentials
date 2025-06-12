ffbcd612b032fcfcac7abf3a2659d77373b92885
Jichen Li <42854324+Roy-Kid@users.noreply.github.com>
2025-06-13 01:32:35 +0200

    Improve element picker layout

--- src/element-picker/plugin.ts
+++ src/element-picker/plugin.ts
@@ -92,19 +92,40 @@ export const ElementPickerBladePlugin: BladePlugin<ElementPickerBladeParams> =
 			return result ? {params: result} : null;
 		},
 		controller(args) {
+			const cols = 18;
+			const rows = 3;
 			const grid = new ButtonGridController(args.document, {
-				size: [18, 3],
+				size: [cols, rows],
 				cellConfig: (x, y) => ({
 					title: ELEMENTS[y][x] ?? '',
 				}),
 			});
+
+			// Hide buttons for empty cells
 			grid.cellControllers.forEach((cc, i) => {
-				const x = i % 18;
-				const y = Math.floor(i / 18);
+				const x = i % cols;
+				const y = Math.floor(i / cols);
 				if (!ELEMENTS[y][x]) {
 					cc.viewProps.set('disabled', true);
+					cc.view.element.style.visibility = 'hidden';
 				}
 			});
+
+			// Adjust column widths based on text length
+			const colWidths = [] as number[];
+			for (let x = 0; x < cols; x++) {
+				let maxLen = 0;
+				for (let y = 0; y < rows; y++) {
+					const sym = ELEMENTS[y][x];
+					if (sym) {
+						maxLen = Math.max(maxLen, sym.length);
+					}
+				}
+				colWidths.push(Math.max(1, maxLen));
+			}
+			grid.view.element.style.gridTemplateColumns = colWidths
+				.map((l) => `${l}fr`)
+				.join(' ');
 			return new ButtonGridBladeController(args.document, {
 				blade: args.blade,
 				labelProps: ValueMap.fromObject<LabelPropsObject>({
